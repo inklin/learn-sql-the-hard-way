f21c8f6ebdf6cafc1c7cd48a2e7bc103f310173f
Stacey Lin <twstacey@gmail.com>
2015-10-18 23:25:03 -0700

    Added Exercise 1-4

---
+++ ex2.sql
@@ -0,0 +1,31 @@
+CREATE TABLE person (
+  id INTEGER PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pet (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT,
+  age INTEGER,
+  dead INTEGER
+);
+
+CREATE TABLE car (
+  id INTEGER PRIMARY KEY,
+  type TEXT,
+  color TEXT,
+  year INTEGER
+);
+
+CREATE TABLE person_pet (
+  person_id INTEGER,
+  pet_id INTEGER
+);
+
+CREATE TABLE person_car (
+  person_id INTEGER,
+  car_id INTEGER
+);
---
+++ ex4.sql
@@ -0,0 +1,4 @@
+# Explicit format
+INSERT INTO person_pet (person_id, pet_id) VALUES (0, 0);
+# Implicit format
+INSERT INTO person_pet VALUES(0, 1);
