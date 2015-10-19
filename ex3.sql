f1018746a8d97bb2a6734a2edb02eef28f299e7d
Stacey Lin <twstacey@gmail.com>
2015-10-19 11:35:00 -0700

    Exercise 3 and 4

--- ex3.sql
+++ ex3.sql
@@ -5,3 +5,9 @@ INSERT INTO pet(id, name, breed, age, dead)
   VALUES (0, "Fluffy", "Unicorn", 1000, 0);
 
 INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
+
+INSERT INTO person(id, first_name, last_name, age)
+  VALUES (1, "Stacey", "Lin", 29);
+
+INSERT INTO pet(id, name, breed, age, dead)
+  VALUES (2, "Drogon", "Dragon", 5, 0);
--- ex4.sql
+++ ex4.sql
@@ -1,4 +1,10 @@
-# Explicit format
-INSERT INTO person_pet (person_id, pet_id) VALUES (0, 0);
-# Implicit format
+/* Explicit entry */
+INSERT INTO person_pet (person_id, pet_id)
+  VALUES (0, 0);
+
+/* Implicit entry */
 INSERT INTO person_pet VALUES(0, 1);
+
+/* Add a person-pet relation for myself */
+INSERT INTO person_pet (person_id, pet_id)
+  VALUES (1, 2);
