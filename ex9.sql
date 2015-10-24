436e481710e82f4d49dcbdaac0d8e264065d8519
Stacey Lin <twstacey@gmail.com>
2015-10-23 23:27:15 -0700

    Exercise 9 Extra Credit

--- ex9.sql
+++ ex9.sql
@@ -6,3 +6,24 @@ UPDATE pet SET name = "Fancy Pants"
 
 SELECT * FROM person;
 SELECT * FROM pet;
+
+UPDATE person SET first_name = "Zed"
+  WHERE person.id = 0;
+
+UPDATE pet SET dead = 1
+  WHERE pet.name = "Drogon";
+
+SELECT * FROM pet;
+
+UPDATE pet SET name = "DECEASED"
+  WHERE pet.dead = 1;
+
+SELECT * FROM pet;
+
+
+UPDATE pet SET name = "Young One" WHERE pet.name IN (
+  SELECT pet.name FROM pet
+  WHERE pet.age < 10
+);
+
+SELECT * FROM pet;
