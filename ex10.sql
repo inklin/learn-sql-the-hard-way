014e01262d12b27cbfca5ff297abcd6928b60075
Stacey Lin <twstacey@gmail.com>
2015-10-24 17:17:47 -0700

    Exercise 10 Extra Credit

--- ex10.sql
+++ ex10.sql
@@ -10,3 +10,14 @@ UPDATE pet SET name = "Zed's Pet" WHERE id IN (
 );
 
 SELECT * FROM pet;
+
+/* Rename dead pets owned by Zed */
+UPDATE pet SET name = "Zed's Dead Pet" WHERE id IN (
+  SELECT pet.id
+  FROM pet, person_pet, person
+  WHERE
+    pet.id = person_pet.pet_id AND
+    person.id = person_pet.person_id AND
+    person.first_name = "Zed" AND
+    pet.dead = 1
+);
